e4e687ae937404dd9e63a5a4f1be7c8113b15642
Sure, Not <13259903+nestak@users.noreply.github.com>
2023-09-14 10:15:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT ? process.env.PORT : 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World #2');
 });
 
 server.listen(port, hostname, () => {
